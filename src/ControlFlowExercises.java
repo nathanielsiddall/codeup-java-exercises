f1d8ff06d78f1139bc5beff2fc8ea5640d833a26
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-04-30 16:58:22 -0500

    still working on the exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        int i = 0;
+        while (i <= 15){
+            i++;
+            System.out.println("you're at number: " + i);
+        }
+        int x = 1;
+        do {
 
+            x *=  2;
+
+            System.out.println(x);
+
+        }while (x <= 100000);
+
+
+        for (i = 0; i <= 100; i++){
+            if (i === ){
+        }
     }
 }
+
